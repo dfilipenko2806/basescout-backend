e784e0bc1a05a81d7bae762d8c03a4b76d3dd167
dfilipenko2806 <154374151+dfilipenko2806@users.noreply.github.com>
2026-03-17 15:20:36 +0300

    Refactor Prediction schema to add new fields

--- models/Prediction.js
+++ models/Prediction.js
@@ -1,31 +1,27 @@
 import mongoose from "mongoose";
 
 const PredictionSchema = new mongoose.Schema({
-
-question: String,
-
-contractId: Number,
-
-rewardPoints: {
-type:Number,
-default:20
-},
-
-endDate: Date,
-
-resolved:{
-type:Boolean,
-default:false
-},
-
-createdAt:{
-type:Date,
-default:Date.now
-}
-
+  question: String,
+  contractId: Number,
+  rewardPoints: {
+    type: Number,
+    default: 20
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  // ===== новые поля для фаз =====
+  playDeadline: Date,   // конец участия
+  resolveTime: Date,    // когда можно показывать результаты
+  resolved: {
+    type: Boolean,
+    default: false
+  },
+  correctChoice: Number
 });
 
 export default mongoose.model(
-"Prediction",
-PredictionSchema
+  "Prediction",
+  PredictionSchema
 );
